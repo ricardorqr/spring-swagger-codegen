dd582e821908a252872170c7a4f6ebed6299a5fa
Ricardo Ribeiro <ricardorqr@gmail.com>
2022-03-24 15:23:12 -0400

    Initial commit

---
+++ src/main/java/com/spring/swagger/codegen/SpringSwaggerCodegenApplication.java
@@ -0,0 +1,13 @@
+package com.spring.swagger.codegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerCodegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerCodegenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/swagger/codegen/SpringSwaggerCodegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.swagger.codegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerCodegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
